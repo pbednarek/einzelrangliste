1588004775a4baebc06afe6421a5eda8e31f5dad
pbednarek <info@pbednarek.de>
2014-03-11 17:08:50 +0100

    Time stuff

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,10 @@
 class ChallengesController < ApplicationController
+  def new
+    @challenge = Challenge.new(
+        challenging_player: current_user,
+        challenged_player: params[:user_id],
+        suggestions:[Time.now, Time.now, Time.now],
+        due_date: (Time.now+14.days).to_date
+    )
+  end
 end
