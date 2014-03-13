8454f9226eb168fbc17def20fe85367dbd3439b9
Christoph Bach <christoph-bach@gmx.de>
2014-03-13 17:04:22 +0100

    view to accept challenge

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Einzelrangliste::Application.routes.draw do
   resources :challenges, except: [:new]
 
   get 'challenges/new/:user_id', to: 'challenges#new', as: 'challenge_user'
+  post 'challenges/:id/accept', to: 'challenges#accept', as: 'challenge_accept'
 end
